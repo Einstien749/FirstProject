d1e7674577ddb75e708181206ea2f0119e06e845
Odimayo Taiye <odimayomoses6@gmail.com>
2022-07-24 02:42:30 +0100

    My First Django App

---
+++ static/home.js
@@ -0,0 +1,6 @@
+function Hidepreloader(){
+    let preloader = document.getElementById("preloader");
+    let body = document.getElementById("main");
+    preloader.style.display = "none";
+    body.style.display = "flex";
+}
